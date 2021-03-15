ac083555730a9451d7362f0a0ca5b223bb345c9c
akhilmhdh <akhilmhdh@gmail.com>
2021-03-15 22:43:18 +0530

    added auth policy and review creation validation

--- api/review/controllers/review.js
+++ api/review/controllers/review.js
@@ -1,8 +1,70 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+const { sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+  async create(ctx) {
+    let entity;
+
+    const user = ctx.state.user.id;
+    const hasReviewedFilter = { user };
+
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+
+      if (data?.activity) {
+        hasReviewedFilter.activity = data?.activity;
+      } else if (data?.destination) {
+        hasReviewedFilter.destination = data?.destination;
+      } else {
+        return ctx.badRequest(null, [
+          { messages: [{ id: "Activity or destination must be given" }] },
+        ]);
+      }
+
+      const hasReviewed = await strapi
+        .query("review")
+        .findOne(hasReviewedFilter);
+
+      if (hasReviewed) {
+        return ctx.badRequest(null, [
+          { messages: [{ id: "User has already reviewed." }] },
+        ]);
+      }
+
+      data.user = user;
+      entity = await strapi.services.review.create(data, { files });
+    } else {
+      const req = ctx.request.body;
+
+      if (req?.activity) {
+        hasReviewedFilter.activity = req.activity;
+      } else if (req?.destination) {
+        hasReviewedFilter.destination = req.destination;
+      } else {
+        return ctx.badRequest(null, [
+          { messages: [{ id: "Activity or destination must be given" }] },
+        ]);
+      }
+
+      const hasReviewed = await strapi
+        .query("review")
+        .findOne(hasReviewedFilter);
+
+      if (hasReviewed) {
+        return ctx.badRequest(null, [
+          { messages: [{ id: "User has already reviewed." }] },
+        ]);
+      }
+
+      req.user = user;
+      entity = await strapi.services.review.create(req);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.review });
+  },
+};
