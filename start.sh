a3667d3016290df400b000906b8adf5a574916a3
gautamkrishnar <r.gautamkrishna@gmail.com>
2021-03-20 16:36:44 +0530

    chore: improved config restore

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 export NODE_ENV=production
+cp dashboard-config.json /tmp/dashboard-config.json
+# Replacing dummy with the actual value
+sed -i "s|googleClientId|$GOOGLE_CLIENT_ID|g" /tmp/dashboard-config.json
+sed -i "s|googleClientSecret|$GOOGLE_CLIENT_SECRET|g" /tmp/dashboard-config.json
+# Restore values
 yarn strapi config:restore -f /tmp/dashboard-config.json
 yarn start
