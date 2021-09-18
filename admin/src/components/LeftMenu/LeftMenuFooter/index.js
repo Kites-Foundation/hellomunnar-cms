757b640e09b8e215cc8549443f8f1e6ffc9b0c66
gautamkrishnar <r.gautamkrishna@gmail.com>
2021-09-18 18:17:44 +0530

    chore: remove unused variables

--- admin/src/components/LeftMenu/LeftMenuFooter/index.js
+++ admin/src/components/LeftMenu/LeftMenuFooter/index.js
@@ -8,11 +8,7 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 import Wrapper, { A } from './Wrapper';
 
-function LeftMenuFooter({ version }) {
-  // PROJECT_TYPE is an env variable defined in the webpack config
-  // eslint-disable-next-line no-undef
-  const projectType = PROJECT_TYPE;
-
+function LeftMenuFooter() {
   return (
     <Wrapper>
       <div className="poweredBy">
